3cb29f50bea50d9032b0e1d83a4bdb7ad81b9604
YooHyeok <yjk7454@naver.com>
2023-03-31 17:02:28 +0900

    타임리프 리터럴 - literal.html추가 및 BasicController /literal Get메서드 추가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -90,5 +90,10 @@ public class BasicController {
         return "/basic/link";
     }
 
+    @GetMapping("literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
 
 }
