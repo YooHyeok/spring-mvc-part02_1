1f71648c05a6688c782e75cede4814c9b3bf2ea7
YooHyeok <yjk7454@naver.com>
2023-04-01 15:05:50 +0900

    타임리프 템플릿레이아웃1 - base.html, layoutMain.html, TemplateController /layout GET메서드 추가

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -10,10 +10,20 @@ public class TemplateController {
 
     /**
      * 타임리프 템플릿 조각
+     * templates/template/fragment/fragmentMain.html
      */
     @GetMapping("fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    /**
+     * 타임리프 템플릿 레이아웃1
+     * templates/template/layout/layoutMain.html
+     */
+    @GetMapping("layout")
+    public String layout() {
+        return "template/layout/layoutMain";
     }
 
 }
